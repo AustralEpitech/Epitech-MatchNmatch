fe3de81347a2b2f7de4ccd8380603a139d925ed8
angeD <ange.duhayon@epitech.eu>
2022-06-05 16:23:33 +0200

    feat: more readable version of match

--- match.c
+++ match.c
@@ -1,5 +1,34 @@
+//int match(const char *s1, const char *s2)
+//{
+//    if (*s2 == '*') {
+//        return match(s1, s2 + 1) || (*s1 && match(s1 + 1, s2));
+//    }
+//    if (*s1 == *s2) {
+//        return 0;
+//    }
+//    if (*s1 == '\0') {
+//        return 1;
+//    }
+//    return match(s1 + 1, s2 + 1);
+//}
+
 int match(const char *s1, const char *s2)
 {
     return (*s2 == '*' && (match(s1, s2 + 1) || (*s1 && match(s1 + 1, s2)))) ||
-        (*s1 == *s2 && (!*s1 || match(s1 + 1, s2 + 1)));
+        (*s1 == *s2 && (*s1 == '\0' || match(s1 + 1, s2 + 1)));
+}
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        return 1;
+    }
+    if (match(argv[1], argv[2])) {
+        printf("Match!\n");
+    }
+    else {
+        printf("No match...\n");
+    }
+    return 0;
 }
